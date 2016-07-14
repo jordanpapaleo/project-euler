aa5fd1e122fc449f64ea083b2b96410fe7175734
Jordan Papaleo <papaleowebdev@gmail.com>
2016-07-14 13:45:48 -0700

    completed

--- problems/004/index.js
+++ problems/004/index.js
@@ -1,51 +1,63 @@
-'use strict'
-
 /*
 A palindromic number reads the same both ways.
 The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
-
-example 999 * 999 = some palindrome
 */
 
+(function() {
+  'use strict'
 
-function checkPalindrome(num) {
-  const forwardArray = `${num}`.split('')
-  const reversedArray = `${num}`.split('').reverse()
+  let result = solve(999, 999)
+  console.log(result)
 
-  let i = forwardArray.length;
-  while(i > 0) {
-    if(forwardArray[i] !== reversedArray[i]) {
-      return false
-    }
+  function checkPalindrome (num) {
+    const forwardArray = `${num}`.split('')
+    const reversedArray = `${num}`.split('').reverse()
 
-    i--
-  }
+    let i = forwardArray.length
 
-  return true
-}
-
-let numA = 999;
-let numB = 999;
-
-var largestNum = 0;
-
-while (numA > 100) {
-  while (numB > 100) {
-    let product = numA * numB
-    if (checkPalindrome(product)) {
-      if(product > largestNum) {
-        largestNum = product;
+    while(i > 0) {
+      if(forwardArray[i] !== reversedArray[i]) {
+        return false
       }
+
+      i--
     }
 
-    numB--
+    return true
   }
 
-  numA--
-  numB = numA
-}
+  function solve (numA, numB) {
+    let largestNum = 0;
+    let iterations = 0;
 
-console.log('***********')
-console.log(largestNum)
+    while (numA) {
+      while (numB) {
+        iterations++;
+
+        let product = numA * numB
+
+        if (product < largestNum) {
+          break;
+        }
+
+        if (checkPalindrome(product)) {
+          if(product > largestNum) {
+            largestNum = product;
+          }
+        }
+
+        numB--
+      }
+
+      numA--
+      numB = numA
+    }
+
+    return {
+      iterations,
+      largestNum
+    }
+  }
+}())
